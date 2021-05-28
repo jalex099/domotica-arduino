8030826adea427abe6a7e79f0fbea4c3e7a79bcf
jalex099 <mm18014@ues.edu.sv>
2021-05-28 12:09:12 -0600

    's'

--- test-server/test/index.js
+++ test-server/test/index.js
@@ -2,13 +2,13 @@ const SerialPort = require("serialport");
 const Readline = SerialPort.parsers.Readline;
 
 const parser = new Readline();
-const mySerial = new SerialPort("COM1",{
+const mySerial = new SerialPort("COM2",{
     baudRate:9600
-})
+},false)
 
-mySerial.on("open",()=>{
+/*mySerial.on("open",()=>{
 console.log("Puerto serial abierto")
-})
+})*/
 
 mySerial.on("data",(data)=>{
     console.log(data)
