52e55e3e7ffa49098137088828c706918dd1deba
jalex099 <mm18014@ues.edu.sv>
2021-05-28 10:13:59 -0600

    First commit in arduino domotica project

---
+++ README.md
@@ -0,0 +1,11 @@
+# PROYECTO DE DOMOTICA MICROPROGRAMACION 2021
+
+Javier Alexander Morales Melara 
+Erick Josué Saravia Aragón
+Keny Lizbeth Chávez Torres
+
+##Instrucciones
+
+Para que el proyecto de proteus funcione, se debe abrir el IDE Arduino y compilar el codigo
+que está en la carpeta "arduino-code" (en el IDE debe estar activada la opción de compilacion en 
+los ajustes *Ctrl + ,*) y copiar la dirección del .hex, esa se pega en proteus y listo.
---
+++ arduino-code/TemperaturaLM35.ino
@@ -0,0 +1,29 @@
+int val;                                         //Declaramos variable de tipo entero val
+
+void setup() {
+Serial.begin (9600);                             //Inicializamos la comunicacion serial a 9600bps
+pinMode(A1,INPUT);                               //Asignamos el pin A1 como entrada (Sensor de Temperatura)
+pinMode(6,OUTPUT);                               //Asignamos el pin 6 como salida (Led Verde)
+pinMode(7,OUTPUT);                               //Asignamos el pin 7 como salida (Led Rojo)
+}
+
+void loop() {
+val = analogRead (A1);                           //Realiza la lectura del pin A1 (Sensor) y el valor lo guarda en la variable val
+float mv = (val/1024.0)*5000;                    //Declaramos variable de tipo float y operamos para obtener el resultado en celsius (grados centigrados)
+float temp = mv/10;                              //Declaramos variable de tipo float y operamos para obtener el resultado en celsius (grados centigrados)
+
+Serial.print ("TEMPERATURE = ");                 //Imprimimos en pantalla TEMPERATURE =
+Serial.print (temp);                             //Imprimimos el valor de la variable temp
+Serial.print ("*C");                             //Imprimimos en pantalla *C
+Serial.println ();                               //Salto de linea
+
+if(temp>37.6){                                   //Si temp es mayor que 37.6
+    digitalWrite(6,LOW);                         //Apaga el Led Verde
+    digitalWrite(7,HIGH);                        //Prende el Led Rojo
+}
+else{                                            //Si temp NO es mayor que 37.6
+  digitalWrite(6,HIGH);                          //Prende el Led Verde
+  digitalWrite(7,LOW);                           //Apaga el Led Rojo
+  }
+delay(1000);                                     //Espera de 1 segundo
+}
