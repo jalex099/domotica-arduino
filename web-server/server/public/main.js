db07bfd6b6a1bc185ab15edc487d1bcb9660a37e
jalex099 <mm18014@ues.edu.sv>
2021-06-16 11:03:00 -0600

    Agregada una mejor interfaz en la representacion de los datos en la web

--- web-server/server/public/main.js
+++ web-server/server/public/main.js
@@ -1,14 +1,63 @@
 const sockets = io();
 
-sockets.on("data",function (data) {
-    let obj = data;
+//let btnSendData = document.querySelector("#btnSendData");
+let temperature = document.querySelector("#temperature");
+let stateDoorGarden = document.querySelector("#stateDoorGarden");
+let indicationsDoorGarden = document.querySelector("#indicationsDoorGarden");
+let stateDoorGarage = document.querySelector("#stateDoorGarage");
+let indicationsDoorGarage = document.querySelector("#indicationsDoorGarage");
+let alertTemperature = document.querySelector("#alertTemperature");
 
-    const temp = document.getElementById('temperature')
-    temp.innerHTML = `${Math.floor(obj.temperature)} °C ` 
-})
-
-let btnSendData = document.querySelector("#btnSendData");
-
-btnSendData.addEventListener("click",()=>{
+/*btnSendData.addEventListener("click",()=>{
   sockets.emit("led-on", 1)
-})
+})*/
+
+sockets.on("data", function (data) {
+  //OBTENCION DE LA DATA
+  let obj = data;
+
+  //TEMPERATURA
+  temperature.innerHTML = `${Math.floor(obj.temperature)} °C `;
+  if (obj.temperature > 35) {
+    try {
+      temperature.classList.remove("text-blue");
+      temperature.classList.add("text-red");
+      alertTemperature.classList.remove("hidden");
+      alertTemperature.classList.add("show");
+    } catch {}
+  } else {
+    try {
+      temperature.classList.remove("text-red");
+      temperature.classList.add("text-blue");
+      alertTemperature.classList.remove("show");
+      alertTemperature.classList.add("hidden");
+    } catch {}
+  }
+
+  //PUERTA DEL PATIO INTERIOR
+  if (obj.gardenDoor == 0) {
+    stateDoorGarden.innerHTML = "Patio interior: CERRADO";
+    indicationsDoorGarden.innerHTML = "";
+    stateDoorGarden.classList.remove("text-red");
+    stateDoorGarden.classList.add("text-white");
+  } else {
+    stateDoorGarden.innerHTML = "Patio interior: ABIERTO";
+    indicationsDoorGarden.innerHTML =
+      "La puerta del patio interior esta abierta, persona pasando en este momento.";
+    stateDoorGarden.classList.remove("text-white");
+    stateDoorGarden.classList.add("text-red");
+  }
+
+  if (obj.garageDoor == 0) {
+    stateDoorGarage.innerHTML = "Cochera: CERRADA";
+    indicationsDoorGarage.innerHTML = "";
+    stateDoorGarage.classList.remove("text-red");
+    stateDoorGarage.classList.add("text-white");
+  } else {
+    stateDoorGarage.innerHTML = "Cochera: ABIERTA";
+    indicationsDoorGarage.innerHTML =
+      "La puerta de la cochera esta abierta, persona pasando en este momento.";
+    stateDoorGarage.classList.remove("text-white");
+    stateDoorGarage.classList.add("text-red");
+  }
+});
